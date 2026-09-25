e573eee54993d5f8ef7e1ced80bc54bf17660a15
nayathra <nayathra04@gmail.com>
2026-09-26 01:12:50 +0530

    Register Twilio call API

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,7 @@ from app.routes.simulation import router as simulation_router
 from app.routes.sms import router as sms_router
 from app.routes.incident import router as incident_router
 from app.routes.voice import router as voice_router
+from app.routes.call import router as call_router
 from app.routes.assistant import router as assistant_router
 
 
@@ -32,6 +33,7 @@ app.include_router(simulation_router)
 app.include_router(sms_router)
 app.include_router(incident_router)
 app.include_router(voice_router)
+app.include_router(call_router)
 app.include_router(assistant_router)
 
 
